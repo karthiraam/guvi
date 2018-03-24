95b3474ca5079a037954c9314fababc6262bf944
karthiraam <37687403+karthiraam@users.noreply.github.com>
2018-03-24 20:10:50 +0530

    Create sumoffirst.c

---
+++ sumoffirst.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int N,K,sum=0;
+printf("enter the values:");
+scanf("%d\t%d",&N,&K);
+for(int i=1;i<=N;i++)
+{
+printf("%d\n",N);
+}
+for(int j=1;j<=K;j++)
+{
+sum=sum+K;
+}
+printf("sum is %d",sum);
+return 0;
+}
