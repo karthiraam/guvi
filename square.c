9a189f2f02ca14262cf5153131888ec0d365c838
karthiraam <37687403+karthiraam@users.noreply.github.com>
2018-03-24 20:26:35 +0530

    Update square.c

--- square.c
+++ square.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
 {
-int x,y,z;
-scanf("%d %d",&x,&y);
-z=pow(x,y);
-printf("%d",z);
-return 0;
+  int x,y,z;
+  printf("Enter the Number of x and y ");
+  scanf("%d %d",&x,&y);
+  z=pow(x,y);
+  printf("%d ",z);
+  return 0;
 }
