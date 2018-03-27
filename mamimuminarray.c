6ab0f6929c5a072168dbce36660d9853092cf59f
karthiraam <37687403+karthiraam@users.noreply.github.com>
2018-03-27 18:22:41 +0530

    Update mamimuminarray.c

--- mamimuminarray.c
+++ mamimuminarray.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-int a[100],b,element=0;
+int a[100],b,element;
 scanf("%d",&b);
 printf("\nEnter the %d numbers",b);
 for(int i=0;i<b;i++)
 {
   scanf("%d",&a[i]);
 }
-
+element=a[0];
 for(int j=0;j<b;j++)
 {
   if(a[j]>element)
