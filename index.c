af83f34d26f94b9ba2efd9f6596c2744dafaa4b3
karthiraam <37687403+karthiraam@users.noreply.github.com>
2018-03-28 14:38:34 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+char a[100];
+int j;
+clrscr();
+printf("Enter string:");
+for(j=0;j<100;j++)
+{
+scanf("%c",&a[j]);
+if(ch[i]=='k')
+{
+break;
+}
+}
+for(j=0;a[j]!='\0';j++)
+{
+printf("%c",a[j]);
+}
+return 0;
+}
